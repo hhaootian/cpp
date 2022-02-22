c257b8629ca112a89fa1ad5847aa578ae8331ff0
Hao Tian <haot@mail.smu.edu>
2022-02-22 17:04:17 -0600

    Add files via upload

---
+++ src/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void helper()
+{
+    const int const_val = 2;
+    #define day 8
+}
+
+int main()
+{
+    double cnt = 1.9;
+    cout << "day = " << day << endl;
+    // cout << "const_val" << const_val << endl;
+    return 0;
+}
